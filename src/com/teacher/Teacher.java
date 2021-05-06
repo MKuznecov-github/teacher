5dfb73d4a693ce82bfccf11dba0c7af6fd5d8273
Maxim <maxim.kuznetsov@mail.ru>
2021-05-06 21:26:12 +0300

    Получение данных от пользователя #9

--- src/com/teacher/Teacher.java
+++ src/com/teacher/Teacher.java
@@ -1,13 +1,25 @@
 package com.teacher;
 
+import java.util.Scanner;
+
 public class Teacher {
     public static void main(String[] args) {
 
-        int num = 5;
-        int num2 = 7;
-        int num3 = num + num2;
+        System.out.print("Input a hour and minutes: ");
+        Scanner times = new Scanner(System.in);
 
-        System.out.println("Результат:" + num3 * (num + num2));
+        int hours = times.nextInt();
+        int minutes = times.nextInt();
+        times.close();
 
+        if (hours < 0 || hours > 24) {
+            System.out.println("Вы ввели неверные часы");
+            return; }
+        else if (minutes < 0 || minutes > 60) {
+            System.out.println("Вы ввели не верно минуты");
+            return;
+        }
+
+        System.out.printf("Указанное Вами время: %d:%d", hours, minutes);
     }
 }
