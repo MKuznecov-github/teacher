9df2dac7cc3f450532ff4506efb0a0f9167c83d5
Maxim <maxim.kuznetsov@mail.ru>
2021-05-07 13:54:29 +0300

    График работы преподавателя.

--- src/com/teacher/Teacher.java
+++ src/com/teacher/Teacher.java
@@ -12,30 +12,30 @@ public class Teacher {
         int minutes = times.nextInt();
         times.close();
 
+        int time = hours * 100 + minutes;
 
-        if (hours < 0 || hours > 24) {
-            System.out.println("Вы ввели неверные часы");
-            return;
-        } else if (minutes < 0 || minutes > 60) {
-            System.out.println("Вы ввели не верно минуты");
+        String result = " - Вы ввели не верное время";
+        if (time < 0 || time > 2400) {
+            System.out.println(result);
             return;
         }
 
         System.out.printf("Указанное Вами время: %d:%d", hours, minutes);
 
-        int time = hours * 100 + minutes;
-
-        if (time >= 1030 && time <= 1200) {
-            System.out.println(" - Преподаватель занят.");
-            return;
-        } else if (time >= 1340 && time <= 1500) {
-            System.out.println(" - Преподаватель занят.");
-            return;
-        } else if (time >= 1800 && time <= 1930) {
-            System.out.println(" - Преподаватель занят.");
-            return;
+        String work = " - Не рабочее время Преподавателя.";
+        if (time <= 1000 || time >= 2000) {
+            System.out.println(work);
         }
-        System.out.println(" - Преподаватель свободен.");
-        return;
+
+            String result1 = " - Преподаватель свободен";
+
+            if (time >= 1030 && time <= 1200) {
+                result1 = " - Преподаватель занят";
+            } else if (time >= 1340 && time <= 1500) {
+                result1 = " - Преподаватель занят";
+            } else if (time >= 1800 && time <= 1930) {
+                result1 = " - Преподаватель занят";
+            }
+            System.out.println(result1);
     }
-    }
+}
