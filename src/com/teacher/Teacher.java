9ae26961195a0c83b74ebf98d0d7e23afd81b9b6
Maxim <maxim.kuznetsov@mail.ru>
2021-05-07 18:08:55 +0300

    График работы преподавателя_2.

--- src/com/teacher/Teacher.java
+++ src/com/teacher/Teacher.java
@@ -12,8 +12,6 @@ public class Teacher {
         int minutes = times.nextInt();
         times.close();
 
-        int time = hours * 100 + minutes;
-
         if (hours < 0 || hours > 24) {
             System.out.println("Вы ввели неверные часы");
             return;
@@ -23,21 +21,23 @@ public class Teacher {
         }
         System.out.printf("Указанное Вами время: %d:%d", hours, minutes);
 
+        int time = hours * 100 + minutes;
+
         String work = " - Не рабочее время Преподавателя.";
         if (time <= 1000 || time >= 2000) {
             System.out.println(work);
             return;
         }
 
-            String result1 = " - Преподаватель свободен";
+        String result1 = " - Преподаватель свободен";
 
-            if (time >= 1030 && time <= 1200) {
-                result1 = " - Преподаватель занят";
-            } else if (time >= 1340 && time <= 1500) {
-                result1 = " - Преподаватель занят";
-            } else if (time >= 1800 && time <= 1930) {
-                result1 = " - Преподаватель занят";
-            }
+        if (time >= 1030 && time <= 1200) {
+            result1 = " - Преподаватель занят";
+        } else if (time >= 1340 && time <= 1500) {
+            result1 = " - Преподаватель занят";
+        } else if (time >= 1800 && time <= 1930) {
+            result1 = " - Преподаватель занят";
+        }
             System.out.println(result1);
             return;
     }
